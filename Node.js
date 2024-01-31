f3bdecaf8e67ba0acebcf3153fbde821fe86b679
Sithsdoc <jos.sardina@gmail.com>
2024-01-31 16:36:50 -0500

    created quote app

--- Node.js
+++ Node.js
@@ -1,1 +1,7 @@
-Math.random();
+let quotes = [
+  "Luke I am your father-Darth Vader",
+  "Good Anakin good, kill him, kill him now -Palpatine",
+  "You were my brother Anakin-Ben kenobi",
+];
+
+console.log(quotes[Math.floor(Math.random() * 3)]);
---
+++ exercises.js
@@ -0,0 +1,8 @@
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+var combine = arrays.reduce(
+  (accumulator, currentValue) => accumulator,
+  currentValue,
+  initialValue
+);
+console.log(combine);
